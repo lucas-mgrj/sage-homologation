d1b4d0f32e9a780012e33ad511a60923545ae803
lucas-mgrj <79200190+lucas-mgrj@users.noreply.github.com>
2023-08-10 12:46:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-from fastapi import FastAPI
+# ---------------------------------Automalógica---------------------------------
+# Nome do Arquivo: AutoFetchAPI.py
+# Descrição: API de Interface entre SAGE e SISCON
+#
+# Autor: Lucas Nascimento
+# Data: 08/08/2023
+#
+# Versão: 1.0
+#
+# Histórico de Modificações:
+#     1.0 - 27/03/2023 - Esboço de código.
+# Notas:
+#     — Existem dois modos de envio, teste e produção.
+# ------------------------------------------------------------------------------
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+
+# Comando para iniciar o servidor: uvicorn main:app --host localhost --port 8080 --reload
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+
+@app.get('/')
+async def af_root():
+    return {'Status': 'Interface Ativa'}
